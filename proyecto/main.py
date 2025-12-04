fcc4d9442cef74e3002c32a809de82df94dcc4a5
Camilo Diaz <camilo20263@gmail.com>
2025-12-04 17:12:11 -0500

    main

---
+++ proyecto/main.py
@@ -0,0 +1,16 @@
+
+
+def main():
+    while True:
+        #mostrar menu 
+        opcion = input("Selecciona una opcion: ")
+
+        if opcion == "1":
+            pass
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            print("Muchas gracias ")
+            break 
+        else:
+            print("Opcion incorrecta seleccione otra opcion ")
